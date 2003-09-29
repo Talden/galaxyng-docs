47d749023a8b0b6cab2ef208ae40c58e8a058a79
christhecat <>
2003-09-29 13:07:22 +0000

    Validation and accessibility updates.  Added validation and accessibility icons and links.  Changed layout.

--- footer.php
+++ footer.php
@@ -1,5 +1,13 @@
 <!-- begin footer.php -->
 <!-- $Id$ -->
-<p>For release 5.1<br />
+<hr />
+<p>GalaxyNG 6.3.1<br />
 <a href="credits.php">Credits and License</a></p>
+
+<p><a href="index.php">Manuals</a>
+<br /><a href="http://badcheese.com/~steve/phpwiki/index.php/GalaxyPlay">Wiki</a></p>
+
+<p><a href="http://validator.w3.org/check/referer"><img src="valid-xhtml11.png" alt="Valid XHTML 1.1!" height="31" width="88" /></a> <a href="http://bobby.watchfire.com/" title="Bobby's Home Page">
+<img src="approved_a.gif" alt="Bobby WorldWide Approved A" width="88" height="31">
+</a> 
 <!-- end footer.php -->
