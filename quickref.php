8ac3a800bd5661d3894b9605766094674d505c78
christhecat <>
2003-09-29 13:11:36 +0000

    First draft of new quick reference in php format.  Modified layout.  Divided orders into sections by type.  Accessibility and validation updates.

--- quickref.php
+++ quickref.php
@@ -1,100 +1,78 @@
-m4_include(`base.m4')m4_dnl
-m4_include(`general.m4')m4_dnl
-DocStart
-AHeader(`GalaxyNG Quick Reference Manual')
-BodyStart
-m4_include(`mannavigator.m4')m4_dnl
+<?php echo '<?xml version="1.0" encoding="iso-8859-1"?>'; ?> 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
+   "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US">
+
 <!-- $Id$ -->
 
-ATitle(`GalaxyNG Quick Reference Manual')
+	<head>
+		<link rel="stylesheet" href="main.css" />
+		<title>GalaxyNG Quick Reference</title>
+	</head>
 
-ASection(`GalaxyNG release-4-1 (Dec 1999)')
+	<body>
+		<h1>GalaxyNG Quick Reference</h1>
+			<table>
 
-<TABLE>
-<TR><TD COLSPAN=3 ALIGN=CENTER><FONT COLOR="red"><STRONG>General Orders</STRONG></FONT></TD>
-<TR><TD>@ [nation ...]</TD>
-    <TD>Send a message to one or more nations.</TD>
-<TR><TD>= &lt;Your real name&gt;</TD>
-    <TD>Set your real name for inclusion in the Hall of Fame.</TD>
-<TR><TD>A &lt;nation&gt;</TD>
-    <TD>Declare peace to another nation.</TD>
-<TR><TD>B &lt;group&gt; &lt;number of ships&gt;</TD>
-    <TD>Break off a number of ships from a group to form a new group.</TD>
-<TR><TD>C &lt;new name&gt;</TD>
-    <TD>Rename your nation.</TD>
-<TR><TD>D &lt;type name&gt; &lt;drive&gt; &lt;attacks&gt; &lt;weapons&gt; &lt;shields&gt; &lt;cargo&gt;</TD>
-    <TD>Design a new ship type.</TD>
-<TR><TD>E &lt;type name&gt;</TD>
-    <TD>Erease a ship type.</TD>
-<TR><TD>F &lt;nation&gt;</TD>
-    <TD>Find the email address of another nation.</TD>
-<TR><TD>G &lt;group&gt; [number of ships]</TD>
-    <TD>Upgrade the tech levels of a group.</TD>
-<TR><TD>H &lt;group&gt;</TD>
-    <TD>Reverse course of a group.</TD>
-<TR><TD>I &lt;group&gt; &lt;planet&gt; [number of ships]</TD>
-    <TD>Send a group on an intercept mission.</TD>
-<TR><TD>L &lt;group&gt; &lt;cargo type&gt; [number-of-ships] [AMOUNT &lt;amount&gt;]</TD>
-    <TD>Load a group with cargo.</TD>
-<TR><TD>M &lt;x&gt; &lt;y&gt; &lt;size&gt;</TD>
-    <TD>Change the map size of your report.</TD>
-<TR><TD>N &lt;old planet name&gt; &lt;new planet name&gt;</TD>
-    <TD>Rename a planet.</TD>
-<TR><TD>O &lt;option&gt;</TD>
-    <TD>Set a game option.</TD>
-<TR><TD>P &lt;planet&gt; &lt;product&gt;</TD>
-    <TD>Set the production type for a planet.</TD>
-<TR><TD>Q &lt;your nation name&gt;</TD>
-    <TD>Quit the game.</TD>
-<TR><TD>R &lt;origin planet&gt; &lt;cargo type&gt; [destination planet]</TD>
-    <TD>Set a cargo route between two planets.</TD>
-<TR><TD>S &lt;group&gt; &lt;planet&gt; [number of ships]</TD>
-    <TD>Send a groups or a number of ships from a group to a planet.</TD>
-<TR><TD>T &lt;type name&gt; &lt;new name&gt;</TD>
-    <TD>Rename a ship type.</TD>
-<TR><TD>U &lt;group&gt; [number-of-ships] [AMOUNT &lt;amount&gt;]</TD>
-    <TD>Unload cargo from a group.</TD>
-<TR><TD>V &lt;planet&gt;</TD>
-    <TD>Claim ownership over a planet.</TD>
-<TR><TD>W &lt;nation&gt;</TD>
-    <TD>Declare war on a nation.</TD>
-<TR><TD>X &lt;group&gt; [number-of-ships]</TD>
-    <TD>Scrap a group of ships.</TD>
-<TR><TD>Y &lt;passwd&gt;</TD>
-    <TD>Change your password.</TD>
-<TR><TD>Z &lt;new address&gt;</TD>
-    <TD>Change the email address turn reports are sent to.</TD>
-<TR><TD COLSPAN=3 ALIGN=CENTER><FONT COLOR="red"><STRONG>Fleet Orders</STRONG></FONT></TD>
-<TR><TD>B &lt;group&gt; FLEET</TD>
-<TD>Remove Group groupno from it's current fleet</TD>
-</TR>
-
-<TR><TD>D FLEET &lt;fleet name&gt;</TD>
-<TD>Create a fleet called fleetname</TD>
-</TR>
-
-<TR><TD>E &lt;fleet name&gt;</TD>
-<TD>Disband Fleet fleet name.</TD>
-</TR>
-
-<TR><TD>I &lt;fleet&gt; &lt;planet&gt;</TD>
-<TD>Intercept ships at planetname with Fleet fleetname.</TD>
-</TR>
-
-<TR><TD>J &lt;group&gt; &lt;fleet&gt; [number-of-ships]</TD>
-<TD>Add Group groupno to Fleet fleetname. If it is currently with another
-fleet it will be moved to the new fleet.</TD>
-</TR>
-
-<TR><TD>J &lt;fleet 1&gt; &lt;fleet 2&gt;</TD>
-<TD>Merge fleet 1 into fleet 2 leaving fleet 1 empty.</TD>
-</TR>
-
-<TR><TD>S &lt;fleet&gt; &lt;planet&gt;</TD>
-<TD>Send Fleet fleet to planet</TD>
-</TR>
-
-<TR><TD>T &lt;fleet&gt; &lt;new fleet name&gt;</TD>
-<TD>Change the name of the fleet.</TD>
-</TR>
-</TABLE>
+				
+				
+				<tr><th colspan="2">Planet Orders</th></tr>
+					<tr><td>Produce</td><td>&nbsp;<a href="manual.php#ordp">p &lt;planet name&gt; &lt;CAP | MAT | DRIVE | WEAPONS | SHIELDS | CARGO | ship type name&gt;</a></td></tr>
+					<tr><td>Route</td><td>&nbsp;<a href="manual.php#ordr">r &lt;origin planet name&gt; &lt;CAP | COL | EMP | MAT&gt; [destination planet name]</a></td></tr>
+					<tr><td>Victory</td><td>&nbsp;<a href="manual.php#ordv">v &lt;planet name&gt;</a></td></tr>
+					<tr><td>Rename</td><td>&nbsp;<a href="manual.php#ordn">n &lt;current planet name&gt; &lt;new planet name&gt;</a></td></tr>
+			
+				
+				
+				<tr><th colspan="2">Ship Type Orders</th></tr>
+					<tr><td>Design</td><td>&nbsp;<a href="manual.php#ordd">d &lt;name&gt; &lt;drive mass&gt; &lt;attacks&gt; &lt;weapons mass&gt; &lt;shields mass&gt; &lt;cargo mass&gt;</a></td></tr>
+					<tr><td>Rename</td><td>&nbsp;<a href="manual.php#ordt">t &lt;current ship type name&gt; &lt;new ship type name&gt;</a></td></tr>
+					<tr><td>Eliminate</td><td>&nbsp;<a href="manual.php#orde">e &lt;ship type name&gt;</a></td></tr>
+					
+					
+					
+				<tr><th colspan="2">Group Orders</th></tr>
+					<tr><td>Send</td><td>&nbsp;<a href="manual.php#ords">s &lt;group number&gt; &lt;planet name&gt; [number of ships]</a></td></tr>
+					<tr><td>Intercept</td><td>&nbsp;<a href="manual.php#ordi">i &lt;group number&gt; &lt;planet name&gt; [number of ships]</a></td></tr>
+					<tr><td>Load</td><td>&nbsp;<a href="manual.php#ordl">l &lt;group number&gt; &lt;CAP | COL | MAT&gt; [number of ships] [AMOUNT &lt;amount&gt;]</a></td></tr>
+					<tr><td>Unload</td><td>&nbsp;<a href="manual.php#ordu">u &lt;group number&gt; [number of ships] [AMOUNT &lt;amount&gt;]</a></td></tr>
+					<tr><td>Upgrade</td><td>&nbsp;<a href="manual.php#ordg">g &lt;group number&gt; [number of ships]</a></td></tr>
+					<tr><td>Break off</td><td>&nbsp;<a href="manual.php#ordb">b &lt;group number&gt; &lt;number of ships&gt;</a></td></tr>
+					<tr><td>Scrap</td><td>&nbsp;<a href="manual.php#ordx">x &lt;group number&gt; [number of ships]</a></td></tr>
+					
+				
+				
+				<tr><th colspan="2">Fleet Orders</th></tr>
+					<tr><td>Send</td><td>&nbsp;<a href="manual.php#ords">s &lt;fleet name&gt; &lt;planet name&gt;</a></td></tr>
+					<tr><td>Intercept</td><td>&nbsp;<a href="manual.php#ordi">i &lt;fleet name&gt; &lt;planet name&gt;</a></td></tr>
+					<tr><td>Design</td><td>&nbsp;<a href="manual.php#ordd">d FLEET &lt;fleet name&gt;</a></td></tr>
+					<tr><td>Join</td><td>&nbsp;<a href="manual.php#ordj">j &lt;group number&gt; &lt;fleet name&gt; [number of ships]</a></td></tr>
+					<tr><td>Leave</td><td>&nbsp;<a href="manual.php#ordb">b &lt;group number&gt; FLEET</a></td></tr>
+					<tr><td>Eliminate</td><td>&nbsp;<a href="manual.php#orde">e &lt;fleet name&gt;</a></td></tr>
+					<tr><td>Merge</td><td>&nbsp;<a href="manual.php#ordj">j &lt;fleet name&gt; &lt;fleet name&gt;</a></td></tr>
+					<tr><td>Rename</td><td>&nbsp;<a href="manual.php#ordt">t &lt;current fleet name&gt; &lt;new fleet name&gt;</a></td></tr>
+					
+				
+				
+				<tr><th colspan="2">Player Orders</th></tr>
+					<tr><td>Player</td><td>&nbsp;<a href="manual.php#ordE">= &lt;FirstName LastName&gt;</a></td></tr>
+					<tr><td>Race</td><td>&nbsp;<a href="manual.php#ordc">c &lt;new player race name&gt;</a></td></tr>
+					<tr><td>Map</td><td>&nbsp;<a href="manual.php#ordm">m &lt;x&gt; &lt;y&gt; &lt;extent&gt;</a></td></tr>
+					<tr><td>Options</td><td>&nbsp;<a href="manual.php#ordo">o [NO] &lt;anonymous | autounload | battleprotocol | compress | gplus | groupforecast | machinereport | planetforecast | prodtable | routesforecast | shiptypeforecast | sortgroups | txtreport | xmlreport&gt;</a></td></tr>
+					<tr><td>Password</td><td>&nbsp;<a href="manual.php#ordy">y &lt;new password&gt;</a></td></tr>
+					<tr><td>Email</td><td>&nbsp;<a href="manual.php#ordz">z &lt;new email address&gt;</a></td></tr>
+					<tr><td>Quit</td><td>&nbsp;<a href="manual.php#ordq">q &lt;player's race name&gt;</a></td></tr>
+					
+				
+				
+				<tr><th colspan="2">Race Orders</th></tr>
+					<tr><td>Message</td><td>&nbsp;<a href="manual.php#ordA">@ [alien race name...]</a></td></tr>
+					<tr><td>Alliance</td><td>&nbsp;<a href="manual.php#orda">a &lt;alien race name&gt;</a></td></tr>
+					<tr><td>Find email</td><td>&nbsp;<a href="manual.php#ordf">f &lt;alien race name&gt;</a></td></tr>
+					<tr><td>War</td><td>&nbsp;<a href="manual.php#ordw">w &lt;alien race name&gt;</a></td></tr>
+			</table>
+ 		
+ 		<?php include 'footer.php'; ?>
+ 	</body>
+</html> 	
