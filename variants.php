3abafba2f48bc019ade75c89a13a97500d94f0c8
christhecat <>
2003-10-31 03:42:44 +0000

    Moved variants onto a separate page.

---
+++ variants.php
@@ -0,0 +1,49 @@
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?> 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
+   "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US">
+
+<!-- $Id$ -->
+
+	<head>
+		<link rel="stylesheet" href="main.css" />
+		<title>Galaxy Variants</title>
+	</head>
+
+	<body>
+		<h1>Galaxy Variants</h1>
+			<p>Updates or corrections?  Contact <a href="http://sourceforge.net/users/christhecat/">christhecat</a>.</p>
+		
+			<h2>Galaxy</h2>
+				<p>The original version of Galaxy was developed by Russel Wallace somewhere in 1991 or maybe even earlier (V2.9 has a 1991 copyright).  To the best of our knowledge, there are no original-version Galaxy servers active.</p>
+				
+			<h2><a href="index.php">GalaxyNG</p> was created by Frans Slothober and introduced multiple homeworlds and partial bombings.  It is the variant that is most similar to original Galaxy.</p>
+			
+			<h2><a href="http://storm.myrnek.de/galaxy/">Blind Galaxy</a></h2>
+				<p>Howard Bampton created Blind Galaxy, which provides less information to players.</p>
+				
+			<h2><a href="http://galaxy.fzu.cz/galaxywww/">Galaxy PBW</a></h2>
+				<p>Galaxy PBW (play-by-web) is a variant of Blind Galaxy played on the web (instead of by email).</p>
+			
+			<h2><a href="http://explorer.sourceforge.net/">GalaxyExplorer</a></h2>
+				<p>Galaxy Explorer is a variant that is similar to, but not based upon, Blind Galaxy.</p>
+				
+			<h2><a href="http://galaxy.pbem.ru/">GalaxyPlus</a></h2>
+				<p>GalaxyPlus (G+) was developed in Russia.  It helps to know Russian to play GalaxyPlus.</p>
+
+			<h2>GalaxyNT</h2>
+				<p>GalaxyNT was a port of GalaxyNG to WindowsNT.</p>
+				
+			<h2>Blind GalaxyNG</h2>
+				<p>Blind GalaxyNG was a modification of GalaxyNG to create a game similar to Blind Galaxy.</p>
+ 
+			<h2>Galaxy G</p>
+				<p>Galaxy G is extinct.</p>
+				
+			<h2>Galactica and Galaxy/2</h2>
+				<p>Galactica and Galaxy/2 are extinct.  Galaxy/2 was the precursor to Galactica.</p>
+ 		
+ 		<?php include 'footer.php'; ?>
+ 	</body>
+</html> 	
