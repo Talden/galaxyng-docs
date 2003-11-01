dc7241b808cdde806be6d91ac65452051b5da610
christhecat <>
2003-11-01 17:16:01 +0000

    Updates to gpl references

--- credits.php
+++ credits.php
@@ -61,7 +61,7 @@
 				<p>Based on Galaxy V3.52: Copyright 1991-1992 by Russell Wallace, Copyright 1993 the Galaxy PBeM Development Group.</p>
 				<p>This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.</p>
 				<p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.</p>
-				<p>You should have received a copy of the <a href="gnu.txt">GNU General Public License</a> along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA</p>
+				<p>You should have received a copy of the <a href="gpl.txt">GNU General Public License</a> along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA</p>
 
 		<?php include 'footer.php'; ?>
 	</body>
