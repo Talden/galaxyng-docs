1e11546d1e1a36be7234e5e369ba322b53930ac0
christhecat <>
2004-02-20 13:44:47 +0000

    More updates for new release

--- footer.php
+++ footer.php
@@ -1,23 +1,14 @@
 
 <!-- begin footer.php -->
+
 <!-- $Id$ -->
+
 <hr />
 
-<p><?php
-$self = trim($base) . "index.php";
-if ($_SERVER['PHP_SELF'] == $self) {
-?>GalaxyNG 6.6<?php } else { ?><a href="<?php include 'base_url.txt'; ?>">GalaxyNG 6.6</a><?php } ?>	| <?php
-$self = trim($base) . "credits.php";
-if ($_SERVER['PHP_SELF'] == $self) {
-?>Credits and License<?php } else { ?><a href="<?php include 'base_url.txt'; ?>credits.php">Credits and License</a><?php } ?></p>
+<p><?php if ($_SERVER['PHP_SELF'] == "/index.php") { ?>GalaxyNG 6.4<?php } else { ?><a href="/">GalaxyNG 6.6</a><?php } ?> | <?php if ($_SERVER['PHP_SELF'] == "/credits.php") { ?>Credits and License<?php } else { ?><a href="/credits.php">Credits and License</a><?php } ?></p>
 <p><?php echo $title; ?>
 <br /><?php echo "Last modified " . date( "F d, Y", getlastmod() ); ?>
-<br /><?php
-$self = trim($base) . "index.php";
-if ($_SERVER['PHP_SELF'] == $self) {
-	echo "http://galaxyng.sourceforge.net/";
-} else {
-	echo "http://galaxyng.sourceforge.net/$_SERVER[PHP_SELF]";
-} ?></p>
+<br /><?php if ($_SERVER['PHP_SELF'] == "/index.php") { echo "http://galaxyng.sourceforge.net/"; } else { echo "http://galaxyng.sourceforge.net/$_SERVER[PHP_SELF]"; } ?></p>
 <p><a href="http://sourceforge.net/"><img src="http://sourceforge.net/sflogo.php?group_id=48224&amp;type=6" width="210" height="62" alt="SourceForge.net Logo" /></a></p>
+
 <!-- end footer.php -->
