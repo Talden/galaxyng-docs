225d10979a5f4a6d81376dbb4bab27cead063865
christhecat <>
2004-03-03 17:16:36 +0000

    Added Singularity GalaxyNG server.
    Also includes updates for Nine Lives games.
    
    Note that this won't be committed every time I make a little change - only for major changes.  These files are regularly updated on the GalaxyNG web site.  They're included in the main home page.

--- activegames.php
+++ activegames.php
@@ -4,12 +4,16 @@
 <ul>
 	<li><a href="http://galaxy.gesserit.net/">Nine Lives GalaxyNG Server</a>
 		<ul>
+			<li><a href="http://galaxy.gesserit.net/Caterwaul/">Caterwaul</a></li>
 			<li><a href="http://galaxy.gesserit.net/Meow1/">Meow1</a></li>
 			<li><a href="http://galaxy.gesserit.net/Meow2/">Meow2</a></li>
 			<li><a href="http://galaxy.gesserit.net/Meow3/">Meow3</a></li>
 			<li><a href="http://galaxy.gesserit.net/Meow4/">Meow4</a></li>
+			<li><a href="http://galaxy.gesserit.net/Meow5/">Meow5</a></li>
 			<li><a href="http://galaxy.gesserit.net/Morat1/">Morat1</a></li>
 			<li><a href="http://galaxy.gesserit.net/Newcomer1/">Newcomer1</a></li>
+			<li><a href="http://galaxy.gesserit.net/Newcomer2/">Newcomer2</a></li>
+			<li><a href="http://galaxy.gesserit.net/Newcomer3/">Newcomer3</a></li>
 			<li><a href="http://galaxy.gesserit.net/SpaceInvaders/">SpaceInvaders</a></li>
 		</ul>
 	</li>
@@ -17,6 +21,12 @@
 		<ul>
 			<li><a href="http://www.refs-home.net/galaxyng/hitlists/REFVillage4.html">REFVillage4</a></li>
 			<li><a href="http://www.refs-home.net/galaxyng/hitlists/REFVillage5.html">REFVillage5</a></li>
+		</ul>
+	</li>
+	<li><a href="http://www.galaxyplus.org/ngserver.fcgi">Galaxy  &#1089;&#1077;&#1088;&#1074;&#1077;&#1088; Singularity &#1087;&#1086; &#1087;&#1088;&#1072;&#1074;&#1080;&#1083;&#1072;&#1084; NG</a><br /><a href="http://www.galaxyplus.org/ngserver.fcgi?lang=en">Singularity GalaxyNG Server</a>
+		<ul>
+			<li><a href="http://www.galaxyplus.org/ngserver.fcgi">singularity1</a> | <a href="http://www.galaxyplus.org/singularity/singularity1/hitlist.html">singularity1 &#1093;&#1086;&#1076;</a> | <a href="http://www.galaxyplus.org/singularity/singularity1/plot.png">singularity1 &#1075;&#1088;&#1072;&#1092;&#1080;&#1082;</a>
+			<br /><a href="http://www.galaxyplus.org/ngserver.fcgi?lang=en">singularity1</a> | <a href="http://www.galaxyplus.org/singularity/singularity1/hitlist.html">singularity1 score</a> | <a href="http://www.galaxyplus.org/singularity/singularity1/plot.png">singularity1 map</a></li>
 		</ul>
 	</li>
 	<li><a href="http://www.badcheese.com/~steve/index.php?page_title=Galaxy">Webb's Web: Galaxy</a>
--- newgames.php
+++ newgames.php
@@ -6,7 +6,6 @@
 	<li><a href="http://galaxy.gesserit.net/">Nine Lives GalaxyNG Server</a>
 		<ul>
 			<li><a href="http://galaxy.gesserit.net/register.php#Cat">Cat</a></li>
-			<li><a href="http://galaxy.gesserit.net/register.php#Caterwaul">Caterwaul</a></li>
 			<li><a href="http://galaxy.gesserit.net/register.php#CatNap">CatNap</a></li>
 			<li><a href="http://galaxy.gesserit.net/register.php#Cheetah">Cheetah</a></li>
 			<li><a href="http://galaxy.gesserit.net/register.php#Meow">Meow</a></li>
@@ -19,6 +18,15 @@
 			<li><a href="http://www.refs-home.net/galaxyng/index.php?action=games">MiniMe1</a></li>							
 			<li><a href="http://www.refs-home.net/galaxyng/index.php?action=games">REFHordes3</a></li>							
 			<li><a href="http://www.refs-home.net/galaxyng/index.php?action=games">REFVillage6</a></li>
+		</ul>
+	</li>	
+	<li><a href="http://www.galaxyplus.org/ngserver.fcgi">Galaxy  &#1089;&#1077;&#1088;&#1074;&#1077;&#1088; Singularity &#1087;&#1086; &#1087;&#1088;&#1072;&#1074;&#1080;&#1083;&#1072;&#1084; NG</a>
+	<br /><a href="http://www.galaxyplus.org/ngserver.fcgi?lang=en">Singularity GalaxyNG Server</a>
+		<ul>
+			<li><a href="http://www.galaxyplus.org/ngserver.fcgi">singularity2 &#1088;&#1077;&#1075;&#1080;&#1089;&#1090;&#1088;&#1072;&#1094;&#1080;&#1103;</a>
+			<br /><a href="http://www.galaxyplus.org/ngserver.fcgi?lang=en">singularity2 registration</a></li>
+			<li><a href="http://www.galaxyplus.org/ngserver.fcgi">singularity3 &#1088;&#1077;&#1075;&#1080;&#1089;&#1090;&#1088;&#1072;&#1094;&#1080;&#1103;</a>
+			<br /><a href="http://www.galaxyplus.org/ngserver.fcgi?lang=en">singularity3 registration</a></li>
 		</ul>
 	</li>
 </ul>
