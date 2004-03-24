f383a863fb7bd52178e2f56a01c3b39fe886d914
christhecat <>
2004-03-24 01:59:55 +0000

    Removed defunct Giga server, added singularity server, added link to GalaxyNG development mailing lists

--- links.php
+++ links.php
@@ -17,9 +17,11 @@
 
 		<h2>GalaxyNG Servers</h2>
 			<ul>
-				<li><a href="http://www.gioco.net/galaxy/">GalaxyNG GIGA Server</a>, Italian</li>
+				<!--<li><a href="http://www.gioco.net/galaxy/">GalaxyNG GIGA Server</a>, Italian</li>-->
 				<li><a href="http://galaxy.gesserit.net/">Nine Lives GalaxyNG Server</a>, maintained by Chris Shaffer</li>
 				<li><a href="http://www.refs-home.net/galaxyng/index.php">REF's GalaxyNG Site</a>, maintained by Remo Fleckinger</li>
+				<li><a href="http://www.galaxyplus.org/ngserver.fcgi">Galaxy  &#1089;&#1077;&#1088;&#1074;&#1077;&#1088; Singularity &#1087;&#1086; &#1087;&#1088;&#1072;&#1074;&#1080;&#1083;&#1072;&#1084; NG</a></li>
+				<li class="nobullet"><a href="http://www.galaxyplus.org/ngserver.fcgi?lang=en">Singularity GalaxyNG Server</a></li>
 				<li><a href="http://www.badcheese.com/~steve/pbm.shtml">Webb's Web: Galaxy</a>, maintained by Steve Webb</li>
 			</ul>
 			
@@ -37,6 +39,7 @@
 		<h2>Communication</h2>
 			<ul>
 				<li><a href="http://lists.sourceforge.net/lists/listinfo/galaxyng-players">galaxyng-players mailing list</a></li>
+				<li><a href="http://sourceforge.net/mail/?group_id=48224">GalaxyNG development mailing lists</a></li>
 				<li><a href="http://groups.google.com/groups?group=rec.games.pbm">rec.games.pbm - the play by mail newsgroup</a></li>
 			</ul>
 		
