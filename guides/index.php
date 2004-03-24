2e6d254d4f619253854831f1442904fba6532061
christhecat <>
2004-03-24 01:07:33 +0000

    Creating new guides and reports section

---
+++ guides/index.php
@@ -0,0 +1,35 @@
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?> 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
+   "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US">
+
+<!-- $Id$ -->
+
+<?php $title="GalaxyNG Guides and Reports"; ?>
+		
+	<head>
+		<link rel="stylesheet" href="../main.css" />
+		<title>GalaxyNG Guides and Reports</title>
+	</head>
+
+	<body>
+		<?php include '../menu.php'; ?>
+		
+		<h2>Guides</h2>
+		
+		<ul>
+			<li><a href="etiquette.php">Galaxy Etiquette (Gettiquette)</a></li>
+		</ul>
+		
+		<h2>Reports</h2>
+		
+		<p>Below are sample turn reports, orders and commentary from several games.  These can be used to test <a href="http://galaxyview.sourceforge.net/tools.html">turn viewers</a> and to learn more about the game.</p>
+		
+		<ul>
+			<li><a href="daily8.tgz">Daily8</a></li>
+		</ul>
+		
+		<?php include '../footer.php'; ?>
+ 	</body>
+</html> 	
