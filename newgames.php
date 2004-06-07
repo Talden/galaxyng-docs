b44d57cf525c8749d599890c7f431b18e8061268
christhecat <>
2004-06-07 11:25:38 +0000

    Removed Steve's server as he's no longer recruiting games.  Removed Ref's server as it isn't clear if he's going to run more or not.

--- newgames.php
+++ newgames.php
@@ -5,12 +5,11 @@
 	<!--<li><a href="http://www.gioco.net/galaxy/">GalaxyNG GIGA Server</a> (Italian)</li>-->
 	<li><a href="http://galaxy.gesserit.net/">Nine Lives GalaxyNG Server</a>
 		<ul>
-			<li><a href="http://galaxy.gesserit.net/tournament.php">Nine Lives GalaxyNG Tournament</a></li>
-			<li><a href="http://galaxy.gesserit.net/register.php#Cat">Cat</a></li>
-			<li><a href="http://galaxy.gesserit.net/register.php#Cheetah">Cheetah</a></li>
-			<li><a href="http://galaxy.gesserit.net/register.php#Newcomer">Newcomer</a></li>
+			<li><a href="http://galaxy.gesserit.net/register.php#Polecat">Polecat</a></li>
+			<li><a href="http://galaxy.gesserit.net/register.php#Tabby">Tabby</a></li>
 		</ul>
 	</li>
+<!--
 	<li><a href="http://www.refs-home.net/galaxyng/">REF's GalaxyNG Site</a>
 		<ul>							
 			<li><a href="http://www.refs-home.net/galaxyng/index.php?action=games">MiniMe1</a></li>							
@@ -18,6 +17,7 @@
 			<li><a href="http://www.refs-home.net/galaxyng/index.php?action=games">REFVillage7</a></li>
 		</ul>
 	</li>	
+-->
 	<li><a href="http://www.galaxyplus.org/ngserver.fcgi">Galaxy  &#1089;&#1077;&#1088;&#1074;&#1077;&#1088; Singularity &#1087;&#1086; &#1087;&#1088;&#1072;&#1074;&#1080;&#1083;&#1072;&#1084; NG</a></li>
 	<li class="nobullet"><a href="http://www.galaxyplus.org/ngserver.fcgi?lang=en">Singularity GalaxyNG Server</a>
 		<ul>
@@ -26,7 +26,6 @@
 			<li><a href="http://www.galaxyplus.org/ngserver.fcgi">singularity3 &#1088;&#1077;&#1075;&#1080;&#1089;&#1090;&#1088;&#1072;&#1094;&#1080;&#1103;</a></li>
 			<li class="nobullet"><a href="http://www.galaxyplus.org/ngserver.fcgi?lang=en">singularity3 registration</a></li>
 		</ul>
-	</li>
 </ul>
 
 <!-- end newgames.php -->
