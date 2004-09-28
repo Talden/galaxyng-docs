c89beef15d9ce1b147ed6d8646c94b067821f76c
christhecat <>
2004-09-28 20:33:24 +0000

    Updated game lists

--- activegames.php
+++ activegames.php
@@ -5,37 +5,20 @@
 	<li><a href="http://galaxy.gesserit.net/">Nine Lives GalaxyNG Server</a>
 		<ul>
 			<li><a href="http://galaxy.gesserit.net/Cat/">Cat</a></li>
-			<li><a href="http://galaxy.gesserit.net/Caterwaul/">Caterwaul</a></li>
-			<li><a href="http://galaxy.gesserit.net/CatNap/">CatNap</a></li>
-			<li><a href="http://galaxy.gesserit.net/Cheetah/">Cheetah</a></li>
 			<li><a href="http://galaxy.gesserit.net/Distance/">Distance</a></li>
-			<li><a href="http://galaxy.gesserit.net/Meow7/">Meow7</a></li>
-			<li><a href="http://galaxy.gesserit.net/tournament.php">Nine Lives GalaxyNG Tournament</a>: <a href="http://galaxy.gesserit.net/t2/">t2</a>, <a href="http://galaxy.gesserit.net/t3/">t3</a>, <a href="http://galaxy.gesserit.net/t5/">t5</a>, <a href="http://galaxy.gesserit.net/t12/">t12</a></p></li>
-			<li><a href="http://galaxy.gesserit.net/Newcomer1/">Newcomer1</a></li>
-			<li><a href="http://galaxy.gesserit.net/Newcomer2/">Newcomer2</a></li>
-			<li><a href="http://galaxy.gesserit.net/Newcomer3/">Newcomer3</a></li>
-			<li><a href="http://galaxy.gesserit.net/SpaceInvaders/">SpaceInvaders</a></li>
+			<li><a href="http://galaxy.gesserit.net/Giants/">Giants</a></li>
+			<li><a href="http://galaxy.gesserit.net/Meercat/">Meercat</a></li>
+			<li><a href="http://galaxy.gesserit.net/Morat2/">Morat2</a></li>
+			<li><a href="http://galaxy.gesserit.net/Newcomer4/">Newcomer4</a></li>
+			<li><a href="http://galaxy.gesserit.net/tournament.php">Nine Lives GalaxyNG Tournament</a>: <a href="http://galaxy.gesserit.net/t13/">t13</a></p></li>
+			<li><a href="http://galaxy.gesserit.net/Polecat/">Polecat</a></li>
 			<li><a href="http://galaxy.gesserit.net/Standard/">Standard</a></li>
+			<li><a href="http://galaxy.gesserit.net/Tabby/">Tabby</a></li>
 		</ul>
 	</li>
-<!--	<li><a href="http://www.refs-home.net/galaxyng/">REF's GalaxyNG Site</a>
+	<li><a href="http://www.truehonour.com/galaxyng.html">True Honor GalaxyNG Server</a>
 		<ul>
-			<li><a href="http://www.refs-home.net/galaxyng/hitlists/REFVillage4.html">REFVillage4</a></li>
-			<li><a href="http://www.refs-home.net/galaxyng/hitlists/REFVillage5.html">REFVillage5</a></li>
-		</ul>
-	</li> -->
-	<li><a href="http://www.galaxyplus.org/ngserver.fcgi">Galaxy  &#1089;&#1077;&#1088;&#1074;&#1077;&#1088; Singularity &#1087;&#1086; &#1087;&#1088;&#1072;&#1074;&#1080;&#1083;&#1072;&#1084; NG</a></li>
-	<li class="nobullet"><a href="http://www.galaxyplus.org/ngserver.fcgi?lang=en">Singularity GalaxyNG Server</a>
-		<ul>
-			<li><a href="http://www.galaxyplus.org/ngserver.fcgi">singularity1 &#1080;&#1075;&#1088;&#1072;</a> | <a href="http://www.galaxyplus.org/singularity/singularity1/hitlist.html">singularity1 &#1093;&#1086;&#1076;</a> | <a href="http://www.galaxyplus.org/singularity/singularity1/plot.png">singularity1 &#1075;&#1088;&#1072;&#1092;&#1080;&#1082;</a></li>
-			<li class="nobullet"><a href="http://www.galaxyplus.org/ngserver.fcgi?lang=en">singularity1 game</a> | <a href="http://www.galaxyplus.org/singularity/singularity1/hitlist.html">singularity1 score</a> | <a href="http://www.galaxyplus.org/singularity/singularity1/plot.png">singularity1 map</a></li>
-		</ul>
-	</li>
-	<li><a href="http://www.badcheese.com/~steve/index.php?page_title=Galaxy">Webb's Web: Galaxy</a>
-		<ul>
-			<li><a href="http://www.badcheese.com/~steve/BadcheeseFinalDaily/hitlist.html">BadcheeseFinalDaily</a> | <a href="http://www.badcheese.com/~steve/BadcheeseFinalDaily/plot.png">BadcheeseFinalDaily map</a> | <a href="http://www.badcheese.com/~steve/BadcheeseFinalDaily/graph.html">BadcheeseFinalDaily graph</a></li>
-			<li><a href="http://www.badcheese.com/~steve/Hardattack/hitlist.html">Hardattack</a> | <a href="http://www.badcheese.com/~steve/Hardattack/plot.png">Hardattack map</a> | <a href="http://www.badcheese.com/~steve/Hardattack/graph.html">Hardattack graph</a></li>
-			<li><a href="http://www.badcheese.com/~steve/Highlander1/hitlist.html">Highlander1</a> | <a href="http://www.badcheese.com/~steve/Highlander1/plot.png">Highlander1 map</a> | <a href="http://www.badcheese.com/~steve/Highlander1/graph.html">Highlander1 graph</a></li>
+			<li><a href="http://www.truehonour.com/galaxyng.html">Leagues</a></li>
 		</ul>
 	</li>
 </ul>
--- newgames.php
+++ newgames.php
@@ -2,21 +2,12 @@
 <!-- begin newgames.php -->
 
 <ul>
-	<!--<li><a href="http://www.gioco.net/galaxy/">GalaxyNG GIGA Server</a> (Italian)</li>-->
+<!--
 	<li><a href="http://galaxy.gesserit.net/">Nine Lives GalaxyNG Server</a>
 		<ul>
-			<li><a href="http://galaxy.gesserit.net/register.php#Polecat">Polecat</a></li>
-			<li><a href="http://galaxy.gesserit.net/register.php#Tabby">Tabby</a></li>
+			<li><a href="http://galaxy.gesserit.net/register.php#Standard2">Standard2</a></li>
 		</ul>
 	</li>
-<!--
-	<li><a href="http://www.refs-home.net/galaxyng/">REF's GalaxyNG Site</a>
-		<ul>							
-			<li><a href="http://www.refs-home.net/galaxyng/index.php?action=games">MiniMe1</a></li>							
-			<li><a href="http://www.refs-home.net/galaxyng/index.php?action=games">REFHordes3</a></li>							
-			<li><a href="http://www.refs-home.net/galaxyng/index.php?action=games">REFVillage7</a></li>
-		</ul>
-	</li>	
 -->
 	<li><a href="http://www.galaxyplus.org/ngserver.fcgi">Galaxy  &#1089;&#1077;&#1088;&#1074;&#1077;&#1088; Singularity &#1087;&#1086; &#1087;&#1088;&#1072;&#1074;&#1080;&#1083;&#1072;&#1084; NG</a></li>
 	<li class="nobullet"><a href="http://www.galaxyplus.org/ngserver.fcgi?lang=en">Singularity GalaxyNG Server</a>
@@ -26,6 +17,11 @@
 			<li><a href="http://www.galaxyplus.org/ngserver.fcgi">singularity3 &#1088;&#1077;&#1075;&#1080;&#1089;&#1090;&#1088;&#1072;&#1094;&#1080;&#1103;</a></li>
 			<li class="nobullet"><a href="http://www.galaxyplus.org/ngserver.fcgi?lang=en">singularity3 registration</a></li>
 		</ul>
+	<li><a href="http://www.truehonour.com/galaxyng.html">True Honor GalaxyNG Server</a>
+		<ul>
+			<li><a href="http://www.truehonour.com/galaxyng.html">Dueler</a>
+		</ul>
+	</li>
 </ul>
 
 <!-- end newgames.php -->
