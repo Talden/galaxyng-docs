c62bad0d4eeca00f16cd53852b55a0c873630a51
christhecat <>
2004-11-10 02:14:21 +0000

    Updated with new games.

--- activegames.php
+++ activegames.php
@@ -4,21 +4,23 @@
 <ul>
 	<li><a href="http://galaxy.gesserit.net/">Nine Lives GalaxyNG Server</a>
 		<ul>
-			<li><a href="http://galaxy.gesserit.net/Cat/">Cat</a></li>
 			<li><a href="http://galaxy.gesserit.net/Distance/">Distance</a></li>
 			<li><a href="http://galaxy.gesserit.net/Giants/">Giants</a></li>
 			<li><a href="http://galaxy.gesserit.net/Meercat/">Meercat</a></li>
 			<li><a href="http://galaxy.gesserit.net/Morat2/">Morat2</a></li>
 			<li><a href="http://galaxy.gesserit.net/Newcomer4/">Newcomer4</a></li>
-			<li><a href="http://galaxy.gesserit.net/tournament.php">Nine Lives GalaxyNG Tournament</a>: <a href="http://galaxy.gesserit.net/t13/">t13</a></p></li>
+			<li><a href="http://galaxy.gesserit.net/tournament.php">Nine Lives GalaxyNG Tournament</a>: <a href="http://galaxy.gesserit.net/t15/">t15</a></p></li>
 			<li><a href="http://galaxy.gesserit.net/Polecat/">Polecat</a></li>
-			<li><a href="http://galaxy.gesserit.net/Standard/">Standard</a></li>
+			<li><a href="http://galaxy.gesserit.net/Standard2/">Standard2</a></li>
 			<li><a href="http://galaxy.gesserit.net/Tabby/">Tabby</a></li>
+			<li><a href="http://galaxy.gesserit.net/Trinity/">Trinity</a></li>
 		</ul>
 	</li>
 	<li><a href="http://www.truehonour.com/galaxyng.html">True Honor GalaxyNG Server</a>
 		<ul>
-			<li><a href="http://www.truehonour.com/galaxyng.html">Leagues</a></li>
+			<li><a href="http://flagship.dyndns.org/galaxyng/Dueler2main.html">Dueler2</a></li>
+			<li><a href="http://flagship.dyndns.org/galaxyng/Leagues1main.html">Leagues1</a></li>
+			<li><a href="http://flagship.dyndns.org/galaxyng/Newbie1main.html">Newbie1</a></li>
 		</ul>
 	</li>
 </ul>
--- newgames.php
+++ newgames.php
@@ -2,13 +2,11 @@
 <!-- begin newgames.php -->
 
 <ul>
-<!--
 	<li><a href="http://galaxy.gesserit.net/">Nine Lives GalaxyNG Server</a>
 		<ul>
-			<li><a href="http://galaxy.gesserit.net/register.php#Standard2">Standard2</a></li>
+			<li><a href="http://galaxy.gesserit.net/register.php#Heaven">Heaven</a></li>
 		</ul>
 	</li>
--->
 	<li><a href="http://www.galaxyplus.org/ngserver.fcgi">Galaxy  &#1089;&#1077;&#1088;&#1074;&#1077;&#1088; Singularity &#1087;&#1086; &#1087;&#1088;&#1072;&#1074;&#1080;&#1083;&#1072;&#1084; NG</a></li>
 	<li class="nobullet"><a href="http://www.galaxyplus.org/ngserver.fcgi?lang=en">Singularity GalaxyNG Server</a>
 		<ul>
@@ -19,7 +17,9 @@
 		</ul>
 	<li><a href="http://www.truehonour.com/galaxyng.html">True Honor GalaxyNG Server</a>
 		<ul>
-			<li><a href="http://www.truehonour.com/galaxyng.html">Dueler</a>
+			<li><a href="http://www.truehonour.com/galaxyng.html">Dueler3</a></li>
+			<li><a href="http://www.truehonour.com/galaxyng.html">Leagues2</a></li>
+			<li><a href="http://www.truehonour.com/galaxyng.html">Newbie2</a></li>
 		</ul>
 	</li>
 </ul>
