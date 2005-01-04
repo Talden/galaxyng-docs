34987dec588bbbe1630a6b725869cd9019f5ad92
christhecat <>
2005-01-04 02:18:42 +0000

    Added Iron Galaxy server.

--- index.php
+++ index.php
@@ -23,20 +23,15 @@
 			
 			<hr />
 			
-			<h2>New Games</h2>			
+			<h2>Servers</h2>			
 				<p>New games are announced on the <a href="http://lists.sourceforge.net/lists/listinfo/galaxyng-players">galaxyng-players mailing list</a> and on GalaxyNG servers:</p>
-				<?php include 'newgames.php'; ?>
-			
-			<hr />
-			
-			<h2>Active Games</h2>
-				<?php include 'activegames.php'; ?>
-
-			
-			<hr />
-
-			<h2>Completed Games</h2>
-				<p>The deeds and exploits of GalaxyNG champions are chronicled in the <a href="hall.php">Hall of Fame</a>.</p>
+				
+				<ul>
+					<li><a href="http://galaxy.gesserit.net/">Nine Lives GalaxyNG Server</a></li>
+					<li><a href="http://students.washington.edu/phaedrus/irongalaxy/">Iron Galaxy</a></li>
+					<li><a href="http://www.galaxyplus.org/ngserver.fcgi">Galaxy  &#1089;&#1077;&#1088;&#1074;&#1077;&#1088; Singularity &#1087;&#1086; &#1087;&#1088;&#1072;&#1074;&#1080;&#1083;&#1072;&#1084; NG</a><br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://www.galaxyplus.org/ngserver.fcgi?lang=en">Singularity GalaxyNG Server</a></li>
+					<li><a href="http://www.truehonour.com/galaxyng.html">True Honor GalaxyNG Server</a></li>
+				</ul>
 				
 			<hr />
 
