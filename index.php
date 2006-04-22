fee5ef5e9e72293c9ac29d3bb19f7e209f081a77
christhecat <>
2006-04-22 10:35:49 +0000

    Fixed spelling error.

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 					<li><a href="http://galaxy.gesserit.net/">Nine Lives GalaxyNG Server</a></li>
 					<li><a href="http://students.washington.edu/phaedrus/irongalaxy/">Iron Galaxy</a></li>
 					<li><a href="http://www.galaxyplus.org/ngserver.fcgi">Galaxy  &#1089;&#1077;&#1088;&#1074;&#1077;&#1088; Singularity &#1087;&#1086; &#1087;&#1088;&#1072;&#1074;&#1080;&#1083;&#1072;&#1084; NG</a><br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://www.galaxyplus.org/ngserver.fcgi?lang=en">Singularity GalaxyNG Server</a></li>
-					<li><a href="http://www.truehonour.com/galaxyng.html">True Honor GalaxyNG Server</a></li>
+					<li><a href="http://www.truehonour.com/galaxyng.html">True Honour GalaxyNG Server</a></li>
 				</ul>
 				
 			<hr />
